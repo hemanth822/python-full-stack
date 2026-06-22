4d227a5a8138ad5248f0e7976262e5b0e8761e6e
hemanth822 <165179774+hemanth822@users.noreply.github.com>
2026-06-22 12:48:36 +0530

    Add files via upload

---
+++ PFS_Day1.py
@@ -0,0 +1,66 @@
+'''
+procedural language 
+---------------------------
+---> this follows a step-by-step approach, where code is structure into procedures such as function or routines...
+Eg---> c
+
+Object oriented program 
+-----------------------------
+---> this is based on objects and classes..
+
+
+print(89+67)
+n1= 89
+n2= 67
+t=n1+n2
+print(id(n1))
+print(id(n2))
+print(id(t))
+
+--------------------
+what is python?
+	-->Dynamically typed  - We do not need to declare the data type of variable. Python knows the type of data we are passing to the variable.
+	-->Interpreter        - Python executes code line by line , till the error and stops at the error.
+	-->High level	      -	
+why python?
+	--> easy syntax
+	-->cross platform
+	-->wide application
+	-->huge library support
+Tokens
+	--> keywords - they are reserved words in python
+	--> Identifiers - Name given to variables, functions, class
+	--> Literals - values stored in variables --- 34, "hello"
+	--> Operators - +,-,/,*
+	--> Punctuators - (),{},[]
+variable rules :
+	-->A-Z,a-z,_
+	--> Do not start with number 
+		eg: 2num= 2 --❌
+comments :
+	--> single line -- # this is comment
+	--> Multi line --  ''' 
+num = 2
+print(num==2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
